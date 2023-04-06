613056611519e23c485232fb95eb18e28814a4dd
ASK-DONE <knueppelalex@web.de>
2023-04-06 14:48:17 +0200

    zwischenstand

--- script_01.js
+++ script_01.js
@@ -12,12 +12,27 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-output(getSentence(["Ich","bin","Max"],"S"));
-output(getSentence(["Bist","du","Max"],"Q"));
-output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+// output(getSentence(["Ich","bin","Max"],"S"));
+// output(getSentence(["Bist","du","Max"],"Q"));
+// output(getSentence(["Ich","bin"],"E"));
+// output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
 
+output(getSentenceArr([i]))
+function  getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = ""; 
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {S
+            str += arr[i] + PUNCT;   
+        } else {
+            str += arr[i] + GAP;  
+        }
+    }
+    return str; 
+ }
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
